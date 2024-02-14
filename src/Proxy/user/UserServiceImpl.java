d37bf7b3c97544f4403812f0f16d10cdb31754a5
JungCheolHoon <cjswovkdnj12@naver.com>
2024-02-14 21:30:48 +0900

    Proxy Pattern : 실제 객체를 대신하여 그 객체의 동작을 제어하거나 추가적인 기능을 제공하는 패턴 , 스프링의 AOP(@Aspect) 와 관련된 디자인 패턴

--- src/Proxy/user/UserServiceImpl.java
+++ src/Proxy/user/UserServiceImpl.java
@@ -1,13 +1,14 @@
 package Proxy.user;
 
 public class UserServiceImpl implements UserService {
+    private final User user = User.getInstance();
     @Override
     public Object getData(String resource) {
         System.out.println("캐싱되지 않은 데이터");
         if(resource.equals("name")){
-            return User.getInstance().getName();
+            return user.getName();
         }else if(resource.equals("age")){
-            return User.getInstance().getAge();
+            return user.getAge();
         }else{
             throw new RuntimeException();
         }
